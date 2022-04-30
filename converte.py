8283283c33b61043f1d26e5ef16385e639fd3cc2
Gabriel Ribeiro <gdsribeiro@id.uff.br>
2022-04-30 18:20:56 -0300

    Adiciona leitura
    Faz leitura do autômato a partir do arquivo dado

--- converte.py
+++ converte.py
@@ -3,3 +3,6 @@ import sys
 argumentos = sys.argv
 nomeArquivo = argumentos.pop(0)
 
+arquivo = open(nomeArquivo+".txt", 'r') #Abre arquivo
+automato = arquivo.readLines()          #Le arquivo
+arquivo.close()                         #Fecha arquivo
