4ce34141ba49aa579afd39102b886ae6a92f8088
Gabriel Ribeiro <gdsribeiro@id.uff.br>
2022-04-30 18:27:24 -0300

    Define fazConversao()
    Define a função fazConversao, para converter o autômato finito não determinístico em gramatica regular

--- converte.py
+++ converte.py
@@ -1,8 +1,16 @@
 import sys
 
+def fazConversao(automato):
+'''
+  Aqui a gente faz junto no laboratorio ☺
+'''
+
 argumentos = sys.argv
 nomeArquivo = argumentos.pop(0)
 
 arquivo = open(nomeArquivo+".txt", 'r') #Abre arquivo
 automato = arquivo.readLines()          #Le arquivo
 arquivo.close()                         #Fecha arquivo
+
+gramatica = fazConversao(automato) #Converte o automato finito não deterministico em gramatica regular
+
