e26e9bebb4192b50e2e14a9ef131e5e647f4f190
Gabriel Ribeiro <gdsribeiro@id.uff.br>
2022-05-02 12:07:53 -0300

    Retrabalha escrita
    cria função escreve() que printa todo o conteudo de uma lista(automato e gramatica são listas)
    cria indicações

--- converte.py
+++ converte.py
@@ -1,5 +1,9 @@
 import sys
 
+def escreve(texto):
+	for linha in texto:
+		print(linha)
+
 def leAutomato(nomeArquivo):
 	arquivo	 = open(nomeArquivo, 'r')	#Abre arquivo
 	automato = arquivo.readlines()          #Le arquivo
@@ -17,8 +21,11 @@ listaArquivos = argumentos.pop()	#Remove primeiro argumento("converte.py")
 for nomeArquivo in listaArquivos:
 	
 	automato  = leAutomato(nomeArquivo)	#Lê automato a partir de um arquivo
-
+	print("AFND:")
+	escreve(automato)	#Escreve o automato
+	
+	print("-----------------")
+	
 	gramatica = fazConversao(automato)	#Converte o automato finito não deterministico em gramatica regular
-
-	for linha in gramatica:	#Escreve a gramatica
-		print(linha)
+	print("Gramatica:")
+	escreve(gramatica)	#Escreve a gramatica
