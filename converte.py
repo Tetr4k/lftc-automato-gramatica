6408c4c954ad1ff43602020fa6b6479c0b243f48
Gabriel Ribeiro <gdsribeiro@id.uff.br>
2022-05-02 11:56:29 -0300

    Cria função leAutomato()
    retorna uma lista com as linhas do arquivo onde o automato estava definido

--- converte.py
+++ converte.py
@@ -1,5 +1,11 @@
 import sys
 
+def leAutomato(nomeArquivo):
+	arquivo = open(nomeArquivo, 'r')	#Abre arquivo
+	automato = arquivo.readlines()          #Le arquivo
+	arquivo.close()                         #Fecha arquivo
+	return automato
+
 def fazConversao(automato):
 '''
   Aqui a gente faz junto no laboratorio ☺
@@ -8,11 +14,9 @@ def fazConversao(automato):
 argumentos = sys.argv
 nomeArquivo = argumentos.pop()
 
-arquivo = open(nomeArquivo, 'r') #Abre arquivo
-automato = arquivo.readlines()          #Le arquivo
-arquivo.close()                         #Fecha arquivo
+automato = leAutomato(nomeArquivo)	#Lê automato a partir de um arquivo
 
-gramatica = fazConversao(automato) #Converte o automato finito não deterministico em gramatica regular
+gramatica = fazConversao(automato)	#Converte o automato finito não deterministico em gramatica regular
 
-for linha in gramatica: #Escreve a gramatica
+for linha in gramatica:	#Escreve a gramatica
 	print(linha)
