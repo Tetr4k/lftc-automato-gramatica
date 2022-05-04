fc44a8d7b3d98ae67520147b924ee9e730a7925b
Gabriel Ribeiro <gdsribeiro@id.uff.br>
2022-05-04 09:47:02 -0300

    Separa modulos
    Separa funções auxiliares em um modulo separado

--- converte.py
+++ converte.py
@@ -1,30 +1,5 @@
-import sys, os, platform
-
-sistema = platform.system()
-
-def limpaTela():
-	if sistema == "Windows":
-		os.system("cls")
-	else:
-		os.system("clear")	
-
-def escreve(texto):
-	cont=0
-	for linha in texto:
-		cont+=1
-		print(f'\t{cont}| {linha}')
-	print("\t----------------")
-
-def filtraQuebraDeLinha(linha):
-	return linha.replace("\n", "")
-
-def filtraEspacos(linha):
-	return linha.replace(" ", "")
-
-def filtra(linha):
-	linha = filtraQuebraDeLinha(linha)
-	linha = filtraEspacos(linha)
-	return linha
+import sys
+from funcoesAuxiliares import limpaTela, filtra, escreve
 
 def leAutomato(nomeArquivo):
 	if not ".txt" in nomeArquivo:
@@ -32,30 +7,26 @@ def leAutomato(nomeArquivo):
 	arquivo	 = open(nomeArquivo, 'r')	#Abre arquivo
 	automato = arquivo.readlines()          #Le arquivo
 	arquivo.close()                         #Fecha arquivo
-	automatoFiltrado = []
-	for linha in automato:
-		linha = filtra(linha)		#Filtra "\n"s e " "s
-		automatoFiltrado.append(linha)
-	return automatoFiltrado
+	return filtra(automato)			#Filtra "\n"s e espacos
 
 def fazConversao(automato):
-'''
-  Aqui a gente faz junto no laboratorio ☺
-'''
+	#Aqui a gente faz junto no laboratorio ☺
+	return []
+
 
 argumentos    = sys.argv	#Captura argumentos passados
-listaArquivos = argumentos[1::]	#Remove primeiro argumento("converte.py")
+listaArquivos = argumentos[1::]	#Remove primeiro argumento(converte.py)
 
 for nomeArquivo in listaArquivos:
-	
+
 	limpaTela()
 	
 	automato  = leAutomato(nomeArquivo)	#Lê automato a partir de um arquivo
 	print("AFND:")
-	escreve(automato)	#Escreve o automato
+	escreve(automato)
 	
 	gramatica = fazConversao(automato)	#Converte o automato finito não deterministico em gramatica regular
 	print("Gramatica:")
-	escreve(gramatica)	#Escreve a gramatica
+	escreve(gramatica)
 	
 	input("Pressione ENTER para continuar . . .")
---
+++ funcoesAuxiliares.py
@@ -0,0 +1,33 @@
+import platform, os
+
+def limpaTela():
+	#Função para limpar tela
+	if platform.system() == "Windows":
+		os.system("cls")
+	else:
+		os.system("clear")	
+
+def escreve(texto):
+	#Função para escrever um texto formatado
+	cont=0
+	for linha in texto:
+		cont+=1
+		print(f'\t{cont}| {linha}')
+	print("\t----------------")
+
+def filtraQuebraDeLinha(linha):
+	#Função para remover quebras de linha
+	return linha.replace("\n", "")
+
+def filtraEspacos(linha):
+	#Função para remover espaços
+	return linha.replace(" ", "")
+
+def filtra(texto):
+	#Função para filtrar um texto
+	textoFiltrado = []
+	for linha in texto:
+		linha = filtraQuebraDeLinha(linha)
+		linha = filtraEspacos(linha)
+		textoFiltrado.append(linha)
+	return textoFiltrado
